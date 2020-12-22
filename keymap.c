a99f4432609481294a0248fd0e8d9a8f8ac48c60
Ale M <bomberstudios@gmail.com>
2020-12-22 23:58:59 +0100

    do not use macros by now

--- keymap.c
+++ keymap.c
@@ -7,8 +7,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT(
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
-    ___________COLEMAK_HOME_LEFT______________                     ___________COLEMAK_HOME_RIGHT______________
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+    CTL_A,   ALT_R,   CMD_S,   SHT_T,   KC_D,                      KC_H,    SHT_N,   CMD_E,   ALT_I,   CTL_O,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
     KC_ESC,  KC_TAB,  KC_LGUI, L_NUM,   KC_BSPC, KC_LCTL, KC_LALT, SPC_3,   L_SYM,   KC_MINS, KC_QUOT, KC_ENT
   ),
   [_QWERTY] = LAYOUT(
